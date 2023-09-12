d665ceee1302bdd80a6917b26cd9b0aa7c8ec6bf
Prerna-sontakke <144796332+Prerna-sontakke@users.noreply.github.com>
2023-09-12 16:31:09 +0530

    Add files via upload
    online restaurant

---
+++ Vendor.java
@@ -0,0 +1,8 @@
+public class Vendor{
+int id;
+String Name;
+String address;
+long phone;
+String city;
+}
+
---
+++ customer.java
@@ -0,0 +1,9 @@
+public class Customer{
+int id;
+S tring name;
+String deliveryAddress;
+long phone;
+String city;
+}
+
+
